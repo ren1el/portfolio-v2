953c83fb5d19b820b995a3b3850f987a4e741baa
Reniel Ocampo <renielxocampo@gmail.com>
2021-12-24 11:13:04 -0800

    make about divs equal

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -17,28 +17,34 @@ const About = (): JSX.Element => {
             <p>
               Sed interdum id quam et efficitur. Lorem ipsum dolor sit amet, consectetur adipiscing
               elit. Quisque imperdiet ultrices tellus, nec tristique nisi laoreet et. Nullam semper
-              congue ex vitae lobortis. Cras a tincidunt justo. Suspendisse fringilla nunc id lectus
-              semper, eget vestibulum est convallis.
+              congue ex vitae lobortis.
+            </p>
+            <p>
+              Sed interdum id quam et efficitur. Lorem ipsum dolor sit amet, consectetur adipiscing
+              elit. Quisque imperdiet ultrices tellus, nec tristique nisi laoreet et. Nullam semper
+              congue ex vitae lobortis.
             </p>
             <p>
               Vivamus quis mollis magna. Nulla vel mi eros. Class aptent taciti sociosqu ad litora
               torquent per conubia nostra, per inceptos himenaeos. Nam efficitur nisl ac ex mollis,
-              tincidunt accumsan lacus ultricies. Vivamus nec erat sit amet sapien vestibulum
-              blandit. In nulla nulla, semper maximus eros nec, fermentum pretium turpis.
+              tincidunt accumsan lacus ultricies.
             </p>
           </div>
           <div>
             <p>
               Mauris gravida pharetra metus, quis porttitor quam luctus et. Cras vestibulum
               consectetur turpis a venenatis. Vivamus et placerat diam, et mollis neque. Donec diam
-              eros, porta efficitur sem ac, elementum lobortis massa. Maecenas ut enim nulla. Aenean
-              interdum accumsan maximus.
+              eros, porta efficitur sem ac, elementum lobortis massa.
             </p>
             <p>
               Nullam molestie leo convallis, congue ex vel, tempus lectus. Donec in ultrices augue.
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at nibh quis
-              libero vulputate varius. Nulla condimentum felis sit amet erat viverra, eu scelerisque
-              tellus fermentum. Sed maximus at mi vitae malesuada.
+              libero vulputate varius.
+            </p>
+            <p>
+              Nullam molestie leo convallis, congue ex vel, tempus lectus. Donec in ultrices augue.
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at nibh quis
+              libero vulputate varius.
             </p>
           </div>
         </div>
