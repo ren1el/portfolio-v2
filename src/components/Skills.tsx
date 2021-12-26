50cc740f2d9d9327174b04f6e4e9491f6d59002b
Reniel Ocampo <renielxocampo@gmail.com>
2021-12-26 13:41:53 -0800

    finish skills section

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,40 +1,41 @@
+import styles from '../styles/Skills.module.scss'
+
 const Skills = (): JSX.Element => {
+  const skills = [
+    {
+      title: 'Languages',
+      skillList: ['JavaScript', 'TypeScript', 'HTML5', 'CSS3', 'Python', 'Java', 'SQL'],
+    },
+    {
+      title: 'Frameworks & Libraries',
+      skillList: ['React', 'Redux', 'Gatsby', 'Next.js', 'Django', 'Express', 'Bootstrap'],
+    },
+    {
+      title: 'Tools',
+      skillList: ['AWS', 'MongoDB', 'Git', 'Node'],
+    },
+    {
+      title: 'Design',
+      skillList: ['Photoshop', 'Figma', 'Adobe XD'],
+    },
+  ]
+
   return (
     <section className={'section'}>
       <aside className={`aside`}>
         <h2 className={'sectionHeader'}>Skills</h2>
       </aside>
-      <div className={'content'}>
-        <p>
-          Sed interdum id quam et efficitur. Lorem ipsum dolor sit amet, consectetur adipiscing
-          elit. Quisque imperdiet ultrices tellus, nec tristique nisi laoreet et. Nullam semper
-          congue ex vitae lobortis. Cras a tincidunt justo. Suspendisse fringilla nunc id lectus
-          semper, eget vestibulum est convallis. Vivamus quis mollis magna. Nulla vel mi eros. Class
-          aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam
-          efficitur nisl ac ex mollis, tincidunt accumsan lacus ultricies. Vivamus nec erat sit amet
-          sapien vestibulum blandit. In nulla nulla, semper maximus eros nec, fermentum pretium
-          turpis. Mauris gravida pharetra metus, quis porttitor quam luctus et. Cras vestibulum
-          consectetur turpis a venenatis. Vivamus et placerat diam, et mollis neque. Donec diam
-          eros, porta efficitur sem ac, elementum lobortis massa. Maecenas ut enim nulla. Aenean
-          interdum accumsan maximus. Nullam molestie leo convallis, congue ex vel, tempus lectus.
-          Donec in ultrices augue. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          Vestibulum at nibh quis libero vulputate varius. Nulla condimentum felis sit amet erat
-          viverra, eu scelerisque tellus fermentum. Sed maximus at mi vitae malesuada. Sed interdum
-          id quam et efficitur. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
-          imperdiet ultrices tellus, nec tristique nisi laoreet et. Nullam semper congue ex vitae
-          lobortis. Cras a tincidunt justo. Suspendisse fringilla nunc id lectus semper, eget
-          vestibulum est convallis. Vivamus quis mollis magna. Nulla vel mi eros. Class aptent
-          taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam
-          efficitur nisl ac ex mollis, tincidunt accumsan lacus ultricies. Vivamus nec erat sit amet
-          sapien vestibulum blandit. In nulla nulla, semper maximus eros nec, fermentum pretium
-          turpis. Mauris gravida pharetra metus, quis porttitor quam luctus et. Cras vestibulum
-          consectetur turpis a venenatis. Vivamus et placerat diam, et mollis neque. Donec diam
-          eros, porta efficitur sem ac, elementum lobortis massa. Maecenas ut enim nulla. Aenean
-          interdum accumsan maximus. Nullam molestie leo convallis, congue ex vel, tempus lectus.
-          Donec in ultrices augue. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          Vestibulum at nibh quis libero vulputate varius. Nulla condimentum felis sit amet erat
-          viverra, eu scelerisque tellus fermentum. Sed maximus at mi vitae malesuada.
-        </p>
+      <div className={`content ${styles.skillsContent}`}>
+        {skills.map((skill, index) => (
+          <div key={index}>
+            <h3 className={styles.skillTitle}>{skill.title}</h3>
+            <ul className={styles.skillList}>
+              {skill.skillList.map((skill, index) => (
+                <li key={index}>{skill}</li>
+              ))}
+            </ul>
+          </div>
+        ))}
       </div>
     </section>
   )
