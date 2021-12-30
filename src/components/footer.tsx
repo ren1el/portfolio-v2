4f2d8b40655ff3de9e6225776413474ba7497667
Reniel Ocampo <renielxocampo@gmail.com>
2021-12-30 13:08:31 -0800

    finish footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,5 +1,30 @@
+import Socials from './Socials'
+import styles from '../styles/Footer.module.scss'
+
 const Footer = (): JSX.Element => {
-  return <footer className="container">footer</footer>
+  return (
+    <footer className="container">
+      <div className={styles.headingContainer}>
+        <h2 className={styles.heading}>Thanks for stopping by!</h2>
+        <h2 className={styles.heading}>Something clever here probably.</h2>
+      </div>
+      <div className={styles.emailContainer}>
+        <button className={styles.contact}>Email me</button>
+        <h2 className={styles.email}>contact@renielocampo.com</h2>
+      </div>
+      <div className={styles.creditsContainer}>
+        <div className={styles.credits}>
+          <span>
+            Copyright © 2022, <strong>Reniel Ocampo</strong>.
+          </span>
+          <span>
+            Built using <a href={'https://nextjs.org/'}>Next.js</a>.
+          </span>
+        </div>
+        <Socials />
+      </div>
+    </footer>
+  )
 }
 
 export default Footer
