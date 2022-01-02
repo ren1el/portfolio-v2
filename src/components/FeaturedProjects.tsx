5af9371fe179adec0917f1f1b0ffa0cb863d8f4e
Reniel Ocampo <renielxocampo@gmail.com>
2022-01-02 15:38:18 -0800

    finish most of featured projects section

--- src/components/FeaturedProjects.tsx
+++ src/components/FeaturedProjects.tsx
@@ -1,70 +1,56 @@
+import styles from '../styles/FeaturedProjects.module.scss'
+import Image from 'next/image'
+import clayformee from '../../public/images/projects/clayformee.png'
+import animalActions from '../../public/images/projects/animal-actions.png'
+import zyllabi from '../../public/images/projects/zyllabi.png'
+
 const FeaturedProjects = (): JSX.Element => {
+  const projects = [
+    {
+      title: 'Clayformee',
+      description: `A mobile-first e-commerce website for a client’s clay earring business. Faced with a number
+        of design challenges, every decision came together to create an overall chic and pleasant
+        shopping page.`,
+      imgSrc: clayformee,
+    },
+    {
+      title: 'Animal Actions',
+      description: `An Alexa skill that asks kids to follow a narrative story and answer questions using their
+        speech input. I worked with a team to iteratively develop this application and research ways
+        to design voice user interfaces for children, refactoring and adding new features along the
+        way.`,
+      imgSrc: animalActions,
+    },
+    {
+      title: 'Zyllabi',
+      description: `A full stack web application that crowd-sources and organizes course syllabi from UC Irvine.
+        I was particularly interested in API and backend design, and hoping overall to alleviate the
+        stress of signing up for classes.`,
+      imgSrc: zyllabi,
+    },
+  ]
+
   return (
     <section className={'section'}>
       <aside className={`aside`}>
         <h2 className={'sectionHeader'}>Featured Projects</h2>
       </aside>
       <div className={'content'}>
-        <p>
-          Sed interdum id quam et efficitur. Lorem ipsum dolor sit amet, consectetur adipiscing
-          elit. Quisque imperdiet ultrices tellus, nec tristique nisi laoreet et. Nullam semper
-          congue ex vitae lobortis. Cras a tincidunt justo. Suspendisse fringilla nunc id lectus
-          semper, eget vestibulum est convallis. Vivamus quis mollis magna. Nulla vel mi eros. Class
-          aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam
-          efficitur nisl ac ex mollis, tincidunt accumsan lacus ultricies. Vivamus nec erat sit amet
-          sapien vestibulum blandit. In nulla nulla, semper maximus eros nec, fermentum pretium
-          turpis. Mauris gravida pharetra metus, quis porttitor quam luctus et. Cras vestibulum
-          consectetur turpis a venenatis. Vivamus et placerat diam, et mollis neque. Donec diam
-          eros, porta efficitur sem ac, elementum lobortis massa. Maecenas ut enim nulla. Aenean
-          interdum accumsan maximus. Nullam molestie leo convallis, congue ex vel, tempus lectus.
-          Donec in ultrices augue. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          Vestibulum at nibh quis libero vulputate varius. Nulla condimentum felis sit amet erat
-          viverra, eu scelerisque tellus fermentum. Sed maximus at mi vitae malesuada. Sed interdum
-          id quam et efficitur. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
-          imperdiet ultrices tellus, nec tristique nisi laoreet et. Nullam semper congue ex vitae
-          lobortis. Cras a tincidunt justo. Suspendisse fringilla nunc id lectus semper, eget
-          vestibulum est convallis. Vivamus quis mollis magna. Nulla vel mi eros. Class aptent
-          taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam
-          efficitur nisl ac ex mollis, tincidunt accumsan lacus ultricies. Vivamus nec erat sit amet
-          sapien vestibulum blandit. In nulla nulla, semper maximus eros nec, fermentum pretium
-          turpis. Mauris gravida pharetra metus, quis porttitor quam luctus et. Cras vestibulum
-          consectetur turpis a venenatis. Vivamus et placerat diam, et mollis neque. Donec diam
-          eros, porta efficitur sem ac, elementum lobortis massa. Maecenas ut enim nulla. Aenean
-          interdum accumsan maximus. Nullam molestie leo convallis, congue ex vel, tempus lectus.
-          Donec in ultrices augue. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          Vestibulum at nibh quis libero vulputate varius. Nulla condimentum felis sit amet erat
-          viverra, eu scelerisque tellus fermentum. Sed maximus at mi vitae malesuada.
-        </p>
-        <p>
-          Sed interdum id quam et efficitur. Lorem ipsum dolor sit amet, consectetur adipiscing
-          elit. Quisque imperdiet ultrices tellus, nec tristique nisi laoreet et. Nullam semper
-          congue ex vitae lobortis. Cras a tincidunt justo. Suspendisse fringilla nunc id lectus
-          semper, eget vestibulum est convallis. Vivamus quis mollis magna. Nulla vel mi eros. Class
-          aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam
-          efficitur nisl ac ex mollis, tincidunt accumsan lacus ultricies. Vivamus nec erat sit amet
-          sapien vestibulum blandit. In nulla nulla, semper maximus eros nec, fermentum pretium
-          turpis. Mauris gravida pharetra metus, quis porttitor quam luctus et. Cras vestibulum
-          consectetur turpis a venenatis. Vivamus et placerat diam, et mollis neque. Donec diam
-          eros, porta efficitur sem ac, elementum lobortis massa. Maecenas ut enim nulla. Aenean
-          interdum accumsan maximus. Nullam molestie leo convallis, congue ex vel, tempus lectus.
-          Donec in ultrices augue. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          Vestibulum at nibh quis libero vulputate varius. Nulla condimentum felis sit amet erat
-          viverra, eu scelerisque tellus fermentum. Sed maximus at mi vitae malesuada. Sed interdum
-          id quam et efficitur. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
-          imperdiet ultrices tellus, nec tristique nisi laoreet et. Nullam semper congue ex vitae
-          lobortis. Cras a tincidunt justo. Suspendisse fringilla nunc id lectus semper, eget
-          vestibulum est convallis. Vivamus quis mollis magna. Nulla vel mi eros. Class aptent
-          taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam
-          efficitur nisl ac ex mollis, tincidunt accumsan lacus ultricies. Vivamus nec erat sit amet
-          sapien vestibulum blandit. In nulla nulla, semper maximus eros nec, fermentum pretium
-          turpis. Mauris gravida pharetra metus, quis porttitor quam luctus et. Cras vestibulum
-          consectetur turpis a venenatis. Vivamus et placerat diam, et mollis neque. Donec diam
-          eros, porta efficitur sem ac, elementum lobortis massa. Maecenas ut enim nulla. Aenean
-          interdum accumsan maximus. Nullam molestie leo convallis, congue ex vel, tempus lectus.
-          Donec in ultrices augue. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          Vestibulum at nibh quis libero vulputate varius. Nulla condimentum felis sit amet erat
-          viverra, eu scelerisque tellus fermentum. Sed maximus at mi vitae malesuada.
-        </p>
+        {projects.map((project, index) => (
+          <div key={index} className={styles.projectCard}>
+            <div
+              className={`${styles.thumbnailContainer} ${
+                index % 2 === 0 ? styles.left : styles.right
+              }`}
+            >
+              <Image src={project.imgSrc} alt={`${project.title} thumbnail`} />
+            </div>
+            <div className={styles.description}>
+              <h3 className={styles.title}>{project.title}</h3>
+              {project.description}
+            </div>
+          </div>
+        ))}
       </div>
     </section>
   )
