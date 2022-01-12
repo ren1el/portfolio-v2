997c2e3a960af4b5273d78dd2d9e5e53b2610c85
Reniel Ocampo <renielxocampo@gmail.com>
2022-01-11 17:13:00 -0800

    add meta tags + favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,23 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
 import Layout from '../components/Layout'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  // TODO: add canonical tag, description, social meta tags
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        <title>Reniel Ocampo</title>
+        <meta charSet="utf-8" />
+        <meta name="author" content="Reniel Ocampo" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/images/favicon.ico" type="image/x-icon" />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   )
 }
 
