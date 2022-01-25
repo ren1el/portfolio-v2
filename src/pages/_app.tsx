3f59253b91ee1bc050755d16335df23349f6a274
Reniel Ocampo <renielxocampo@gmail.com>
2022-01-24 18:48:48 -0800

    add more meta tags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,14 +4,30 @@ import Layout from '../components/Layout'
 import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  // TODO: add canonical tag, description, social meta tags
   return (
     <>
       <Head>
         <title>Reniel Ocampo</title>
+
         <meta charSet="utf-8" />
+        <meta
+          name="description"
+          content="Reniel Ocampo — I’m a software engineer that likes to design and build clean web experiences. Currently based in Southern California."
+        />
         <meta name="author" content="Reniel Ocampo" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="application-name" content="Reniel Ocampo" />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Reniel Ocampo" />
+        <meta
+          property="og:description"
+          content="Reniel Ocampo — I’m a software engineer that likes to design and build clean web experiences. Currently based in Southern California."
+        />
+        <meta property="og:image" content="/images/thumbnail.png" />
+        <meta property="og:url" content="https://renielocampo.com" />
+        <meta property="og:site_name" content="Reniel Ocampo" />
+
         <link rel="icon" href="/images/favicon.ico" type="image/x-icon" />
       </Head>
       <Layout>
